60dd18850cc1d869514136a9bb74d8482035fbca
Emad <emadaghajanzade@yahoo.com>
2022-12-11 11:52:03 +0100

    Fifth Edition

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from subscribe.models import Subscribe
 # Create your views here.
 
 def subscribe(request):
-    context = {}
+    email_error = ""
+    if request.POST:
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        email = request.POST['email']
+        print(f"post REQ: {email}")
+        if email == "":
+            email_error = "Email field is empty"
+
+        subscribe = Subscribe(first_name = first_name, last_name = last_name, email = email)
+        subscribe.save()
+    context = {"email_error": email_error}
     return render(request, 'subscribe/subscribe.html', context)
